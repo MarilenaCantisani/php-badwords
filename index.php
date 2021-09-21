eb8d708b946f47743f4430b55d8ebf3ce0a059f9
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-21 14:45:32 +0200

    Variable addition with a paragraph of text

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+<?php 
+    // Variable with a text paragraph
+    $sentence = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic assumenda accusamus ex dolor, officia eius ipsam illo accusantium omnis eveniet labore ullam voluptas quibusdam! Earum tempore dolorem odit saepe possimus.";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BADWORDS</title>
+    <title>Bad words</title>
 </head>
 <body>
-    
+    <!-- Title -->
+    <h1>
+        Bad words
+    </h1>
 </body>
 </html>
